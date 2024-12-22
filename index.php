32cb197ed1acdd4287732e4fe4b51c61767cd79e
kiana4481 <kiana.vahediyan@gmail.com>
2024-12-22 15:55:14 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>asdasdasd</h1>
+   
+
+    <?php
+    echo "<table border=1>";
+    for($i=1;$i<10;$i++)
+    {
+        echo "<tr>";
+        for($j=1;$j<10;$j++)
+        {
+            echo "<td>".$i*$j."</td>";
+        }
+
+        echo "</tr>";
+    }
+    echo "</table>"
+
+?> 
+
+
+<script>
+
+</script>
+</body>
+</html>
